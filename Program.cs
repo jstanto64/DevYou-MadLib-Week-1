b05434a69591cad62b08be8f13007094e997c056
jstanto64 <73408019+jstanto64@users.noreply.github.com>
2020-10-30 15:36:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1MadlLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is declaring and initializing a varible
+            string adjective1 = "wild";
+           
+            //This is declaring and initializing a varible
+            string noun1 = "bird";
+
+            //This is declaring and initializing a varible
+            string verbp1 = "flew";
+
+            //This is declaring and initializing a varible
+            string adverb1 = "fast";
+
+            //This is declaring and initializing a varible
+            string adjective2 = "large";
+
+            //This is declaring and initializing a varible
+            string noun2 = "home";
+
+            //This is declaring and initializing a variable
+            string noun3 = "tray";
+
+            //This is declaring and initializing a variable
+            string adjective3 = "double";
+
+            //This is declaring and initializing a variable
+            string verb = "run";
+
+            //This is declaring and initializing a variable
+            string adverb2 = "very fast";
+
+            //This is declaring and initializing a variable
+            string verbp2 = "asked";
+
+            //This is declaring and initializing a variable
+            string adjective4 = "full";
+
+
+            Console.WriteLine("Today I went to the zoo. I saw a " + adjective1  + noun1  + 
+                " jumping up and down in its tree. He " + verbp1  + adverb1  + " through the" +
+                " large tunnel that led to its " + adjective2  + noun2  + ". I got some" +
+                " peanuts and passed them through the cage to a gigantic" +
+                " grey " + noun3  + " towering above my head. Feeding that animal made" +
+                " me hungry. I went to get a " + adjective3  + "scoop of ice cream. " +
+                " It filled my stomach. Afterwards I had to " + verb  + adverb2  + " to" +
+                " catch our bus. When I got home I " + verbp2 + " my mom for " 
+                + adjective4 + " day at the zoo.");
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
